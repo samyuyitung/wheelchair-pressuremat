cf599b3f1a36b0e7d2c82a9d2fe2c96b97ff388a
Sam Yuyitung <samyuyitung@gmail.com>
2019-04-15 17:18:49 +0800

    Readme and updates'

--- heatmap.pde
+++ heatmap.pde
@@ -1,23 +1,27 @@
 import processing.serial.*;
 Serial port;
-
 int NUM = 4;
 final color MIN_COLOR = color(255, 255, 255);
 final color MAX_COLOR = color(255, 0, 0);
 
+final int MIN_CAP = 200;
+final int MAX_CAP = 400;
+
 void setup() {
   size(500, 500);
   frameRate(60);
-  port = new Serial(this, "/dev/cu.usbmodem14401", 9600);
+  port = new Serial(this, "/dev/cu.usbmodem14601", 9600);
   port.bufferUntil('\n');
 }
 
 void draw() {
   int[][] data = fetchData();
+
   if (data != null) {
     for (int i = 0; i < data.length; i++) {
-      for (int j = 0; j < data.length; j++) {
-        fill(lerpColor(MIN_COLOR, MAX_COLOR, data[i][j] / 1023.0));
+      for (int j = 0; j < data[0].length; j++) {
+		float scaledCap = map(data[i][j], MIN_CAP, MAX_CAP, 0,1);
+        fill(lerpColor(MIN_COLOR, MAX_COLOR, scaledCap);
         rect(i * width / NUM, j * height / NUM, width / NUM, height / NUM);
       }
     }
@@ -28,15 +32,21 @@ void draw() {
 int[][] fetchData() {
   String serial = fetchFromSerial();
   if (serial != null) {
-    int[][] data = new int[4][4];
+    int[][] data = new int[NUM][NUM];
+
     String[] rows = split(serial, ';');
-    for (int i = 0; i < rows.length; i++) {
-      String[] cols = split(rows[i], ',');
-      for (int j = 0; j < cols.length; j++) {
-        data[i][j] = int(cols[j]);
+
+    if (rows.length == NUM) {
+      for (int i = 0; i < rows.length; i++) {
+        String[] cols = split(rows[i], ',');
+        if (cols.length == NUM) {
+          for (int j = 0; j < cols.length; j++) {
+            data[i][j] = int(trim(cols[j]));
+          }
+        }
       }
     }
-    
+
     return data;
   }
   return null;
--- muxCode/muxCode.ino
+++ muxCode/muxCode.ino
@@ -15,31 +15,51 @@ unsigned long elapsedTime;
 long capacitance;
 
 void setup() {
-  pinMode(CHARGE_PIN, OUTPUT);
-  digitalWrite(CHARGE_PIN, LOW);
-
-
   pinMode(MUX_CTRL_A, OUTPUT);
   pinMode(MUX_CTRL_B, OUTPUT);
   pinMode(MUX_CTRL_C, OUTPUT);
-
-
+  pinMode(CHARGE_PIN, OUTPUT);
+  
+  digitalWrite(CHARGE_PIN, LOW);
   Serial.begin(9600);
 }
 
 void loop() {
+  samplePin(ANALOG_PIN_1);
+  
+  Serial.print(";");
+  
+  samplePin(ANALOG_PIN_2);
+  
+  Serial.println();
+}
+
+void sampleMux(int pin) {
   for (int i = 0; i < 8; i++) {
     writeControl(i);
-    Serial.print(measureCapacitance(ANALOG_PIN_1));
-    Serial.print(",");
-    Serial.print(measureCapacitance(ANALOG_PIN_2));
-    if (i % 2 == 0) {
+    Serial.print(measureCapacitance(pin));
+    if (i == 3) {
       Serial.print(";");
     } else if (i < 7) {
       Serial.print(",");
     }
   }
-  Serial.println();
+}
+
+int measureCapacitance(int analogPin) {
+  digitalWrite(CHARGE_PIN, LOW);
+  pinMode(DISCHARGE_PIN, OUTPUT);
+  digitalWrite(DISCHARGE_PIN, LOW);
+  while (analogRead(analogPin) > 0) { /* nothing */ }
+
+  pinMode(DISCHARGE_PIN, INPUT);
+
+  startTime = micros();
+  digitalWrite(CHARGE_PIN, HIGH);
+  while (analogRead(analogPin) < 648) { /* nothing */ }
+  
+  elapsedTime = micros() - startTime;
+  return ((float)elapsedTime / RESISTOR) * 1000000.0;
 }
 
 void writeControl(int input) {
@@ -48,21 +68,3 @@ void writeControl(int input) {
   digitalWrite(MUX_CTRL_C, HIGH && (input & B00000100));
 }
 
-int measureCapacitance(int analogPin) {
-  digitalWrite(CHARGE_PIN, LOW);
-  pinMode(DISCHARGE_PIN, OUTPUT);
-  digitalWrite(DISCHARGE_PIN, LOW);
-  while (analogRead(analogPin) > 0) {
-    /* nothing */
-  }
-
-  pinMode(DISCHARGE_PIN, INPUT);
-
-  startTime = micros();
-  digitalWrite(CHARGE_PIN, HIGH);
-  while (analogRead(analogPin) < 648) {
-    /* Nothing */
-  }
-  elapsedTime = micros() - startTime;
-  return ((float)elapsedTime / RESISTOR) * 1000000.0;
-}
